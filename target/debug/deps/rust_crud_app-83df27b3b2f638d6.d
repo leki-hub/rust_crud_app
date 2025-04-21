430a92dcc72557215e1c0ca0bd14f6c4d3d412fe
Leki <lekisintole82@gmail.com>
2025-04-21 23:49:59 +0300

    dependencies cargo.toml file

---
+++ target/debug/deps/rust_crud_app-83df27b3b2f638d6.d
@@ -0,0 +1,6 @@
+C:\Users\Solar Panda\Desktop\RUSTCRUD\rust_crud_app\target\debug\deps\librust_crud_app-83df27b3b2f638d6.rmeta: src\main.rs src\db.rs
+
+C:\Users\Solar Panda\Desktop\RUSTCRUD\rust_crud_app\target\debug\deps\rust_crud_app-83df27b3b2f638d6.d: src\main.rs src\db.rs
+
+src\main.rs:
+src\db.rs:
---
+++ target/debug/deps/rust_crud_app-98eb0ac15f496644.d
@@ -0,0 +1,6 @@
+C:\Users\Solar Panda\Desktop\RUSTCRUD\rust_crud_app\target\debug\deps\librust_crud_app-98eb0ac15f496644.rmeta: src\main.rs src\db.rs
+
+C:\Users\Solar Panda\Desktop\RUSTCRUD\rust_crud_app\target\debug\deps\rust_crud_app-98eb0ac15f496644.d: src\main.rs src\db.rs
+
+src\main.rs:
+src\db.rs:
